b202eac679a6e7e1ad4b2c0b6f6a8575f2d9b7fc
李娜(Allen) <allenAip@gmail.com>
2024-12-24 00:17:13 +0800

    feat:新增 DateFilterParent 接口，用于自定义数据权限拦截

---
+++ alinesno-infra-base-gateway-rest/src/main/java/com/alinesno/infra/base/gateway/rest/aop/DateFilterParent.java
@@ -0,0 +1,24 @@
+//package com.alinesno.infra.base.gateway.rest.aop;
+//
+//
+//import com.alinesno.infra.base.authority.entity.ManagerAccountEntity;
+//
+//import java.util.Map;
+//
+///**
+// * 自定义数据权限拦截
+// *
+// * @author  luoxiaodong
+// * @since 2020年3月22日 下午5:56:47
+// */
+//public interface DateFilterParent {
+//
+//	/**
+//	 * 前端参数
+//	 *
+//	 * @param paramMap 参数
+//	 * @param account  当前登录用户
+//	 */
+//	void filterMap(Map<String, Object> paramMap, ManagerAccountEntity account);
+//
+//}

330ff38af3575701f52f63a3c43c0a2efd6575bd
李娜(Allen) <allenAip@gmail.com>
2024-12-24 00:17:13 +0800

    feat:在Client实体类中新增状态及备注字段

--- alinesno-infra-base-gateway-core/src/main/java/com/alinesno/infra/base/gateway/core/entity/Client.java
+++ alinesno-infra-base-gateway-core/src/main/java/com/alinesno/infra/base/gateway/core/entity/Client.java
@@ -22,26 +22,33 @@ import java.util.Date;
 @Table(name="client")
 @Data
 public class Client extends BaseEntity implements java.io.Serializable {
+
     @Id
     private String id;
+
     @NotNull(message = "客户端系统代号不能为空")
     @Size(min = 2, max = 40, message = "网关路由系统代号长度必需在2到40个字符内")
     @Column(name = "systemCode" )
     private String systemCode;
+
     @NotNull(message = "客户端名称不能为空")
     @Size(min = 2, max = 40, message = "网关路由名称长度必需在2到40个字符内")
     @Column(name = "name")
     private String name;
+
     @NotNull(message = "客户端分组不能为空")
     @Column(name = "groupCode")
     private String groupCode;
+
     @Column(name = "ip")
     private String ip;
+
     /**
      * 状态，0是启用，1是禁用
      */
     @Column(name = "status")
     private String status;
+
     @Column(name = "remarks")
     private String remarks;
     /**
@@ -52,6 +59,7 @@ public class Client extends BaseEntity implements java.io.Serializable {
     @NotNull(message = "创建时间不能为空")
     @Column(name = "createTime")
     private Date createTime;
+
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @Column(name = "updateTime")
     private Date updateTime;

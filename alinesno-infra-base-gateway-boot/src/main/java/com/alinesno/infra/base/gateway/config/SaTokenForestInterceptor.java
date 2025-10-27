f5f5a7501034cd3f7faac07d28ae6bad5238bb7a
张婷(Tina) <tinaAip@gmail.com>
2025-10-28 00:23:24 +0800

    feat:添加SaTokenForestInterceptor拦截器，用于添加Authorization请求头

---
+++ alinesno-infra-base-gateway-boot/src/main/java/com/alinesno/infra/base/gateway/config/SaTokenForestInterceptor.java
@@ -0,0 +1,24 @@
+package com.alinesno.infra.base.gateway.config;
+
+import cn.dev33.satoken.stp.StpUtil;
+import com.dtflys.forest.http.ForestRequest;
+import com.dtflys.forest.interceptor.Interceptor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Forest拦截器
+ */
+@Slf4j
+public class SaTokenForestInterceptor<T> implements Interceptor<T> {
+
+    /**
+     * 该方法在请求发送之前被调用, 若返回false则不会继续发送请求
+     * @Param request Forest请求对象
+     */
+    @Override
+    public boolean beforeExecute(ForestRequest req) {
+        req.addHeader("Authorization", "Bearer " + StpUtil.getTokenValue());
+        return true;  // 继续执行请求返回true
+    }
+
+}

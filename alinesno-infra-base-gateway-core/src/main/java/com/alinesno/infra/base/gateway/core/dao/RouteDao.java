ae98b858203570a087f7749f93599c7bd093dc80
张婷(Tina) <tinaAip@gmail.com>
2025-10-28 00:23:24 +0800

    fix:添加RouteDao.java中缺失的import语句

--- alinesno-infra-base-gateway-core/src/main/java/com/alinesno/infra/base/gateway/core/dao/RouteDao.java
+++ alinesno-infra-base-gateway-core/src/main/java/com/alinesno/infra/base/gateway/core/dao/RouteDao.java
@@ -1,10 +1,10 @@
 package com.alinesno.infra.base.gateway.core.dao;
 
-import java.util.List;
-
 import com.alinesno.infra.base.gateway.core.entity.Route;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 /**
  * @description 网关服务Dao数据层操作接口

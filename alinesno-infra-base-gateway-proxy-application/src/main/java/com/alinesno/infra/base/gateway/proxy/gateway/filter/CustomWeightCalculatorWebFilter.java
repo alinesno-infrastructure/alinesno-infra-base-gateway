06b142a918f91cc3d26fef05ab02ed07fa8492b9
张婷(Tina) <tinaAip@gmail.com>
2025-10-28 00:23:25 +0800

    feat:在CustomWeightCalculatorWebFilter中添加权重清理事件监听

--- alinesno-infra-base-gateway-proxy-application/src/main/java/com/alinesno/infra/base/gateway/proxy/gateway/filter/CustomWeightCalculatorWebFilter.java
+++ alinesno-infra-base-gateway-proxy-application/src/main/java/com/alinesno/infra/base/gateway/proxy/gateway/filter/CustomWeightCalculatorWebFilter.java
@@ -1,15 +1,6 @@
 package com.alinesno.infra.base.gateway.proxy.gateway.filter;
 
-import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.WEIGHT_ATTR;
-
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
-
+import com.alinesno.infra.base.gateway.proxy.gateway.event.WeightRemoveApplicationEvent;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.ObjectProvider;
@@ -28,26 +19,13 @@ import org.springframework.validation.Validator;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
-
-/*
- * Copyright 2013-2020 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-import com.alinesno.infra.base.gateway.proxy.gateway.event.WeightRemoveApplicationEvent;
-
 import reactor.core.publisher.Mono;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.WEIGHT_ATTR;
 
 /**
  * @description 此过滤器是直接复制gateway中的WeightCalculatorWebFilter过滤器源代码的基础上，增加了WeightRemoveApplicationEvent监听事件，

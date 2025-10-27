63090e6440f21f4d1a35b950881487c7793f3375
梁明树(Nina) <ninaAip@gmail.com>
2025-10-28 00:23:24 +0800

    feat:优化产品侧边栏菜单，增加仪表盘入口并调整菜单项描述

--- alinesno-infra-base-gateway-ui/src/layout/components/ProductSidebar/index.vue
+++ alinesno-infra-base-gateway-ui/src/layout/components/ProductSidebar/index.vue
@@ -1,16 +1,40 @@
 <template>
   <div class="siderbar">
     <el-menu default-active="1" class="el-menu-vertical" :collapse="isCollapse" @open="handleOpen" @close="handleClose">
-      <el-menu-item index="1" @click="jumpTo">
-        <i class="fa-solid fa-desktop"></i>
-      </el-menu-item>
+      <!-- <el-menu-item index="1" @click="jumpTo">
+        <i class="fa-solid fa-computer"></i>
+      </el-menu-item> -->
 
-      <el-tooltip effect="dark" :content="item.desc" v-for="item in menuItems" :key="item.id" placement="right">
-          <el-menu-item :index="item.id" @click="openServiceList(item.link)">
+      <!-- <el-tooltip effect="dark" :content="item.desc" v-for="item in menuItems" :key="item.id" placement="right"> -->
+          <el-menu-item
+              :index="item.id"
+              @click="openServiceList(item.link)"
+              v-for="item in menuItems"
+              :key="item.id"
+              class="aip-menu-item">
             <i :class="item.icon"></i>
+            <span>
+              {{ item.desc }}
+            </span>
           </el-menu-item>
-      </el-tooltip>
+      <!-- </el-tooltip> -->
 
+    </el-menu>
+
+    <el-menu style="" class="el-menu-vertical acp-suggest" :collapse="isCollapse" @open="handleOpen" @close="handleClose">
+      <!-- <el-tooltip effect="dark" :content="item.desc" v-for="item in footerMenuItems" :key="item.id" placement="right"> -->
+          <el-menu-item
+            :index="item.id"
+            v-for="item in footerMenuItems"
+            :key="item.id"
+            @click="openServiceList(item.link)"
+            class="aip-menu-item">
+            <i :class="item.icon"></i>
+            <span>
+              {{ item.desc }}
+            </span>
+          </el-menu-item>
+      <!-- </el-tooltip> -->
     </el-menu>
 
   </div>
@@ -23,12 +47,13 @@ const router = useRouter();
 
 // 菜单列表
 const menuItems = ref([
+  { id: '19', icon: 'fa-solid fa-computer', link: '/index', desc: '仪盘表' },
   {id:'2' , icon:'fa-solid fa-feather' , link:'/loadBalanced' , desc:'负载管理'},
-  {id:'3' , icon:'fa-solid fa-masks-theater' , link:'/gatewayList' , desc:'网关服务管理'},
-  {id:'4' , icon:'fa-solid fa-server' , link:'/clientList' , desc:'客户端管理'},
-  {id:'5' , icon:'fa-solid fa-plane-departure' , link:'/ipList' , desc:'IP名单管理'},
+  {id:'3' , icon:'fa-solid fa-masks-theater' , link:'/gatewayList' , desc:'网关'},
+  {id:'4' , icon:'fa-solid fa-server' , link:'/clientList' , desc:'客户端'},
+  {id:'5' , icon:'fa-solid fa-plane-departure' , link:'/ipList' , desc:'IP名单'},
   {id:'7' , icon:'fa-solid fa-house-tsunami' , link:'/apiCount' , desc:'接口统计'},
-  {id:'8' , icon:'fa-solid fa-swatchbook' , link:'/apiMonitor' , desc:'接口监控'},
+  {id:'8' , icon:'fa-solid fa-swatchbook' , link:'/apiMonitor' , desc:'监控'},
   // {id:'9' , icon:'fa-solid fa-file-pdf' , link:'/apiDoc' , desc:'接口文档'},
 ]);
 
@@ -54,6 +79,7 @@ function openSmartService() {
 
 </script>
 
+
 <style lang="scss" scoped>
 .el-menu-vertical:not(.el-menu--collapse) {
   width: 65px;
@@ -73,5 +99,22 @@ function openSmartService() {
   overflow: hidden;
   background-color: #fff;
   position: fixed;
+}
+
+.aip-menu-item {
+
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  line-height: 1.2rem;
+  padding-top: 0px;
+  margin: 5px;
+  border-radius: 10px;
+  justify-content: center;
+
+  span{
+    font-size:12px;
+    color: #888;
+  }
 }
 </style>

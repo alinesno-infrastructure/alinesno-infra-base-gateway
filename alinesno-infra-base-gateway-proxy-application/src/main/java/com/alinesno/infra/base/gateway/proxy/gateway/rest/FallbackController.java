947925f15e34400ffae10dc3cfe864bd77cd052e
李伟(Michael) <michaelAip@gmail.com>
2025-10-29 00:20:23 +0800

    fix:修正导入的类路径，修复编译错误。

--- alinesno-infra-base-gateway-proxy-application/src/main/java/com/alinesno/infra/base/gateway/proxy/gateway/rest/FallbackController.java
+++ alinesno-infra-base-gateway-proxy-application/src/main/java/com/alinesno/infra/base/gateway/proxy/gateway/rest/FallbackController.java
@@ -1,7 +1,7 @@
 package com.alinesno.infra.base.gateway.proxy.gateway.rest;
 
+import com.alinesno.infra.base.gateway.adapter.dto.ApiResult;
 import com.alinesno.infra.base.gateway.core.entity.Route;
-import com.alinesno.infra.base.gateway.core.util.ApiResult;
 import com.alinesno.infra.base.gateway.core.util.Constants;
 import com.alinesno.infra.base.gateway.proxy.gateway.cache.RouteCache;
 import lombok.extern.slf4j.Slf4j;

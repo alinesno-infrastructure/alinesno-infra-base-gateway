c62e3d4e7f23c5556eb1f3b40b43a9e9f86849ca
李伟(Michael) <michaelAip@gmail.com>
2025-10-29 00:20:23 +0800

    refactor:调整导入包路径，使用适配层的DTO和ApiResult类。

--- alinesno-infra-base-gateway-proxy-application/src/main/java/com/alinesno/infra/base/gateway/proxy/gateway/rest/DynamicConfigController.java
+++ alinesno-infra-base-gateway-proxy-application/src/main/java/com/alinesno/infra/base/gateway/proxy/gateway/rest/DynamicConfigController.java
@@ -1,7 +1,7 @@
 package com.alinesno.infra.base.gateway.proxy.gateway.rest;
 
-import com.alinesno.infra.base.gateway.core.dto.GatewayConfigDTO;
-import com.alinesno.infra.base.gateway.core.util.ApiResult;
+import com.alinesno.infra.base.gateway.adapter.dto.ApiResult;
+import com.alinesno.infra.base.gateway.adapter.dto.GatewayConfigDTO;
 import com.alinesno.infra.base.gateway.proxy.gateway.service.ConfigRefreshService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;

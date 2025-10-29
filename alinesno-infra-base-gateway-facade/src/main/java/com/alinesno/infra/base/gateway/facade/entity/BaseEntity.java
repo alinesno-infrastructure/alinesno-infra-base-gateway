6a98e8049da07978f083098f7ed56bad163ab05b
张婷(Tina) <tinaAip@gmail.com>
2025-10-30 02:34:39 +0800

    feat:创建BaseEntity实体类，包含operatorId和orgId属性

---
+++ alinesno-infra-base-gateway-facade/src/main/java/com/alinesno/infra/base/gateway/facade/entity/BaseEntity.java
@@ -0,0 +1,25 @@
+package com.alinesno.infra.base.gateway.facade.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.validation.constraints.Min;
+import lombok.Data;
+
+/**
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+@Data
+@MappedSuperclass
+public class BaseEntity {
+
+    @Min(value = 2 , message = "2个字符以上")
+    @Column(name = "operatorId" )
+    private Long operatorId;
+
+    @Min(value = 2 , message = "2个字符以上")
+    @Column(name = "orgId" , nullable = true)
+    private Long orgId ; // 组织id
+
+}

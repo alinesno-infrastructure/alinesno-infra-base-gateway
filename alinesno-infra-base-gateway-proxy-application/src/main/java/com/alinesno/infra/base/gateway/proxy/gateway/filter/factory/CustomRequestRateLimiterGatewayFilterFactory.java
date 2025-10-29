5f96336f119698f6fec54b7a6b62468a6c640884
张婷(Tina) <tinaAip@gmail.com>
2025-10-30 02:34:41 +0800

    refactor:调整导入包路径，优化代码结构

--- alinesno-infra-base-gateway-proxy-application/src/main/java/com/alinesno/infra/base/gateway/proxy/gateway/filter/factory/CustomRequestRateLimiterGatewayFilterFactory.java
+++ alinesno-infra-base-gateway-proxy-application/src/main/java/com/alinesno/infra/base/gateway/proxy/gateway/filter/factory/CustomRequestRateLimiterGatewayFilterFactory.java
@@ -2,9 +2,9 @@ package com.alinesno.infra.base.gateway.proxy.gateway.filter.factory;
 
 import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson.JSONObject;
-import com.alinesno.infra.base.gateway.adapter.dto.ApiResult;
-import com.alinesno.infra.base.gateway.core.util.Constants;
 import com.alinesno.infra.base.gateway.core.util.HttpResponseUtils;
+import com.alinesno.infra.base.gateway.facade.constant.Constants;
+import com.alinesno.infra.base.gateway.facade.dto.ApiResult;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.gateway.filter.GatewayFilter;

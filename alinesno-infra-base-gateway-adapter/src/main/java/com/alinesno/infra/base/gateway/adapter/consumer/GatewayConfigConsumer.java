6aa50d1780860426784f47c9cfe696b074b60377
张婷(Tina) <tinaAip@gmail.com>
2025-10-30 02:34:42 +0800

    refactor:调整GatewayConfigConsumer接口的注解配置，添加拦截器和超时设置

--- alinesno-infra-base-gateway-adapter/src/main/java/com/alinesno/infra/base/gateway/adapter/consumer/GatewayConfigConsumer.java
+++ alinesno-infra-base-gateway-adapter/src/main/java/com/alinesno/infra/base/gateway/adapter/consumer/GatewayConfigConsumer.java
@@ -1,7 +1,7 @@
 package com.alinesno.infra.base.gateway.adapter.consumer;
 
-import com.alinesno.infra.base.gateway.adapter.dto.ApiResult;
-import com.alinesno.infra.base.gateway.adapter.dto.GatewayConfigDTO;
+import com.alinesno.infra.base.gateway.facade.dto.ApiResult;
+import com.alinesno.infra.base.gateway.facade.dto.GatewayConfigDTO;
 import com.dtflys.forest.annotation.BaseRequest;
 import com.dtflys.forest.annotation.JSONBody;
 import com.dtflys.forest.annotation.Post;
@@ -10,7 +10,11 @@ import com.dtflys.forest.annotation.Post;
  * @author luoxiaodong
  * @version 1.0.0
  */
-@BaseRequest(baseURL = "#{alinesno.infra.base.gateway.proxy.host}")
+@BaseRequest(
+        baseURL = "#{alinesno.infra.base.gateway.proxy.host}" ,
+        interceptor = {SaTokenForestInterceptor.class} ,
+        connectTimeout = 30*1000
+)
 public interface GatewayConfigConsumer {
 
     @Post("/api/base/gateway/proxy/configuration/change")

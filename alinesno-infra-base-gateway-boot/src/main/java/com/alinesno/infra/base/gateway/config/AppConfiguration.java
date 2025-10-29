9e841f28cbcf9bb479c3a4597c83eed8dd176167
张婷(Tina) <tinaAip@gmail.com>
2025-10-30 02:34:42 +0800

    feat:引入EnableEncryptableProperties注解用于加密配置属性

--- alinesno-infra-base-gateway-boot/src/main/java/com/alinesno/infra/base/gateway/config/AppConfiguration.java
+++ alinesno-infra-base-gateway-boot/src/main/java/com/alinesno/infra/base/gateway/config/AppConfiguration.java
@@ -2,6 +2,7 @@ package com.alinesno.infra.base.gateway.config;
 
 import com.alinesno.infra.common.web.adapter.sso.enable.EnableInfraSsoApi;
 import com.dtflys.forest.springboot.annotation.ForestScan;
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -13,6 +14,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
         "com.alinesno.infra.base.gateway.adapter.consumer"
 })
 @EnableScheduling
+@EnableEncryptableProperties
 @Configuration
 public class AppConfiguration {
 }

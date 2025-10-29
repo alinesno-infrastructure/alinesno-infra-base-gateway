7e41023c4df36d465dae2071f208c7af6996860c
张婷(Tina) <tinaAip@gmail.com>
2025-10-30 02:34:19 +0800

    feat:创建RouteReq类，包含路由相关属性和分页参数。

---
+++ alinesno-infra-base-gateway-facade/src/main/java/com/alinesno/infra/base/gateway/facade/bean/RouteReq.java
@@ -0,0 +1,31 @@
+package com.alinesno.infra.base.gateway.facade.bean;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * @description
+ * @author jianglong
+ * @date 2020/05/11
+ * @version v1.0.0
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class RouteReq extends BaseReq implements java.io.Serializable  {
+
+    //表单值
+    private RouteFormBean form;
+    //过滤器开关
+    private RouteFilterBean filter;
+    //熔断器开关
+    private RouteHystrixBean hystrix;
+    //限流器开关
+    private RouteLimiterBean limiter;
+    //鉴权器开关
+    private RouteAccessBean access;
+    //监控开关
+    private MonitorBean monitor;
+
+    private Integer currentPage;
+    private Integer pageSize;
+}

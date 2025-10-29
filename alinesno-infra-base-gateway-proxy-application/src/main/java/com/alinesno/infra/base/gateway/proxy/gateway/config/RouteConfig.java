fd79933c041e79ce8a38fee3e80a03e92e5296fd
张婷(Tina) <tinaAip@gmail.com>
2025-10-30 02:34:42 +0800

    refactor:调整RouteConfig.java中的导入包路径

--- alinesno-infra-base-gateway-proxy-application/src/main/java/com/alinesno/infra/base/gateway/proxy/gateway/config/RouteConfig.java
+++ alinesno-infra-base-gateway-proxy-application/src/main/java/com/alinesno/infra/base/gateway/proxy/gateway/config/RouteConfig.java
@@ -2,9 +2,9 @@ package com.alinesno.infra.base.gateway.proxy.gateway.config;
 
 
 import com.alinesno.infra.base.gateway.core.config.ApplicationContextProvider;
-import com.alinesno.infra.base.gateway.core.dao.RouteDao;
-import com.alinesno.infra.base.gateway.core.entity.Route;
-import com.alinesno.infra.base.gateway.core.util.Constants;
+import com.alinesno.infra.base.gateway.facade.constant.Constants;
+import com.alinesno.infra.base.gateway.facade.entity.Route;
+import com.alinesno.infra.base.gateway.domain.dao.RouteDao;
 import com.alinesno.infra.base.gateway.proxy.gateway.cache.RouteCache;
 import com.alinesno.infra.base.gateway.proxy.gateway.filter.factory.AuthorizeGatewayFilterFactory;
 import com.alinesno.infra.base.gateway.proxy.gateway.service.LoadRouteService;

8994b09fb16fbafbb4f3a1009bbb7ef1dcd86969
张婷(Tina) <tinaAip@gmail.com>
2025-10-30 02:34:42 +0800

    refactor:调整导入包路径，优化代码结构

--- alinesno-infra-base-gateway-proxy-application/src/main/java/com/alinesno/infra/base/gateway/proxy/gateway/rest/FallbackController.java
+++ alinesno-infra-base-gateway-proxy-application/src/main/java/com/alinesno/infra/base/gateway/proxy/gateway/rest/FallbackController.java
@@ -1,8 +1,8 @@
 package com.alinesno.infra.base.gateway.proxy.gateway.rest;
 
-import com.alinesno.infra.base.gateway.adapter.dto.ApiResult;
-import com.alinesno.infra.base.gateway.core.entity.Route;
-import com.alinesno.infra.base.gateway.core.util.Constants;
+import com.alinesno.infra.base.gateway.facade.constant.Constants;
+import com.alinesno.infra.base.gateway.facade.dto.ApiResult;
+import com.alinesno.infra.base.gateway.facade.entity.Route;
 import com.alinesno.infra.base.gateway.proxy.gateway.cache.RouteCache;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;

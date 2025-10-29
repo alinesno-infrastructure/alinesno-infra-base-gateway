d0b1b9cdb8072918af7fd77e121c8ec198e6db50
张婷(Tina) <tinaAip@gmail.com>
2025-10-30 02:34:10 +0800

    feat:创建RegServerReq类，继承RegServer并添加分页参数

---
+++ alinesno-infra-base-gateway-facade/src/main/java/com/alinesno/infra/base/gateway/facade/bean/RegServerReq.java
@@ -0,0 +1,18 @@
+package com.alinesno.infra.base.gateway.facade.bean;
+
+import com.alinesno.infra.base.gateway.facade.entity.RegServer;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * @description
+ * @author jianglong
+ * @date 2020/05/16
+ * @version v1.0.0
+ */
+@Data
+@EqualsAndHashCode(callSuper=false)
+public class RegServerReq extends RegServer implements java.io.Serializable {
+    private Integer currentPage;
+    private Integer pageSize;
+}

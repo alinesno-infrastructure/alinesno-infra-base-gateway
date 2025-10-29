96f8b3d5de0bb0640638d7b855eee0a7fc1e27e3
张婷(Tina) <tinaAip@gmail.com>
2025-10-30 02:34:31 +0800

    feat:创建SecureIpReq类用于接收前端请求参数

---
+++ alinesno-infra-base-gateway-facade/src/main/java/com/alinesno/infra/base/gateway/facade/bean/SecureIpReq.java
@@ -0,0 +1,18 @@
+package com.alinesno.infra.base.gateway.facade.bean;
+
+import com.alinesno.infra.base.gateway.facade.entity.SecureIp;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * @description 对前端请求进行接收与封装bean
+ * @author jianglong
+ * @date 2020/05/28
+ * @version v1.0.0
+ */
+@Data
+@EqualsAndHashCode(callSuper=false)
+public class SecureIpReq extends SecureIp implements java.io.Serializable {
+    private Integer currentPage;
+    private Integer pageSize;
+}

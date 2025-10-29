8bf19e9e392c4c000e4c906a8273517ef32b612b
张婷(Tina) <tinaAip@gmail.com>
2025-10-30 02:34:30 +0800

    feat:创建统计常量类StatisticalDataConstant，定义统计相关常量

---
+++ alinesno-infra-base-gateway-facade/src/main/java/com/alinesno/infra/base/gateway/facade/constant/StatisticalDataConstant.java
@@ -0,0 +1,10 @@
+package com.alinesno.infra.base.gateway.facade.constant;
+
+/**
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public class StatisticalDataConstant {
+    public final static String STATISTICAL_TOTAL_KEY = "TOTAL-KEY";
+    public final static String STATISTICAL_DAY_KEY = "DAY-KEY:";
+}

c903128760aea6dfdc969ae7ec27ac12211076df
张婷(Tina) <tinaAip@gmail.com>
2025-10-30 02:34:39 +0800

    feat:创建RouteRsp类，继承Route并添加monitor属性

---
+++ alinesno-infra-base-gateway-facade/src/main/java/com/alinesno/infra/base/gateway/facade/bean/RouteRsp.java
@@ -0,0 +1,18 @@
+package com.alinesno.infra.base.gateway.facade.bean;
+
+import com.alinesno.infra.base.gateway.facade.entity.Monitor;
+import com.alinesno.infra.base.gateway.facade.entity.Route;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * @description
+ * @author JL
+ * @date 2021/04/16
+ * @version v1.0.0
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class RouteRsp extends Route {
+    private Monitor monitor;
+}
